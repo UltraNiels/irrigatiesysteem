73c651a622c44280ece4c351495cf2c70562e5da
Niels <niels.i.huisman@gmail.com>
2021-06-06 22:45:48 +0200

    split to config and data

--- pump.py
+++ pump.py
@@ -5,12 +5,12 @@ from time import sleep
 with open('config.yaml', r) as cf:
 	c = yaml.load(cf, Loader=yaml.FullLoader)
 
-pin = c['config']['pump_relay_pin']
+pin = c['pump_relay_pin']
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
 
 GPIO.output(18, GPIO.HIGH) # turn on pump
-sleep(c['config']['pump_time'] / 100)
+sleep(c['pump_time'] / 1000)
 GPIO.output(18, GPIO.LOW) # turn off pump
 
